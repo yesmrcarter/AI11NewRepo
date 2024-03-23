3f8cfeb44fb0d33e0fe5f146b313695bfdb22aee
root <thisismrcarter@gmail.com>
2024-03-23 23:03:35 +0000

    added two instance and change the Name of another

--- main.tf
+++ main.tf
@@ -2,12 +2,34 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "NewIntance" {
+resource "aws_instance" "New-Intance" {
   count = 1
   ami = "ami-02d7fd1c2af6eead0"
   instance_type = "t2.medium"
   key_name = "Triage_keypair"
   tags = {
-    Name = "Actionitem11"
+    Name = "NewInstance"
     }
 }
+
+
+resource "aws_instance" "Rhel-Instance" {
+    
+    ami   = "ami-0fe630eb857a6ec83"
+    instance_type = "t2.medium"
+    key_name = "Triage_keypair"
+    tags = {
+      Name = "NewRhel"
+      }
+}
+
+resource "aws_instance" "Ubuntu-Intance" {
+  count = 1
+  ami = "ami-0cd59ecaf368e5ccf"
+  instance_type = "t2.medium"
+  key_name = "Triage_keypair"
+  tags = {
+    Name = "NewUbuntu"
+    }
+}
+  
